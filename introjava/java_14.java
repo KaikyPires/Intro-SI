a242e07aae11453cd74085b0f5de014da3240d10
Kaiky Pires <129606756+KaikyPires@users.noreply.github.com>
2023-08-22 18:22:33 -0300

    Add files via upload

---
+++ introjava/java_14.java
@@ -0,0 +1,27 @@
+
+import java.util.Random;
+
+
+public class java_14 {
+
+    public static void main(String[] args) {
+       int m[][] = new int [4][4];
+       int soma = 0;
+        Random gerador = new Random();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+             m[i][j] = gerador.nextInt(15);
+                System.out.print( m[i][j]+" ");
+                if (i == j) {
+                    soma = soma + m[i][j];
+                    
+                }
+                
+                
+            }
+             System.out.println(" ");
+        }
+        System.out.println(soma);
+    }
+    
+}
